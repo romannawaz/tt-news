cc9bf144442e07ade442289fbe671811c30962c3
romannawaz <romannawaz01@gmail.com>
2023-07-12 19:03:45 +0300

    feat: create-news page

--- src/app/pages/create-news/create-news.component.ts
+++ src/app/pages/create-news/create-news.component.ts
@@ -1,8 +1,64 @@
 import { Component } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+import { first } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
+
+export interface NewsForm {
+  title: FormControl<string>;
+  description: FormControl<string>;
+}
 
 @Component({
   selector: 'app-create-news',
   templateUrl: './create-news.component.html',
   styleUrls: ['./create-news.component.scss'],
 })
-export class CreateNewsComponent {}
+export class CreateNewsComponent {
+  faCheck = faCheck;
+  faXMark = faX;
+
+  newsForm: FormGroup<NewsForm> = this.fb.group({
+    title: this.fb.control('', [Validators.required]),
+    description: this.fb.control('', [Validators.required]),
+  });
+
+  createdSuccess: boolean = false;
+  createdFailure: boolean = false;
+
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private newsService: NewsService
+  ) {}
+
+  createNews() {
+    if (!this.newsForm.valid) {
+      this.createdFailure = true;
+
+      return;
+    }
+
+    const data = this.newsForm.getRawValue();
+
+    this.newsService
+      .createNews({ ...data, date: Date.now().toString(), id: uuidv4() })
+      .pipe(first())
+      .subscribe({
+        complete: () => {
+          this.newsForm.reset();
+          this.createdSuccess = true;
+          this.createdFailure = false;
+        },
+        error: () => {
+          this.createdFailure = true;
+          this.createdSuccess = false;
+        },
+      });
+  }
+}
--- src/app/pages/create-news/create-news.module.ts
+++ src/app/pages/create-news/create-news.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreateNewsRoutingModule } from './create-news-routing.module';
 import { CreateNewsComponent } from './create-news.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [CreateNewsComponent],
-  imports: [CommonModule, CreateNewsRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    CreateNewsRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    FontAwesomeModule,
+  ],
 })
 export class CreateNewsModule {}
