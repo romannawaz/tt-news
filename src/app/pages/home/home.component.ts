9398b8aea0e480256ea26c471571df17f8975a52
romannawaz <romannawaz01@gmail.com>
2023-07-12 16:39:48 +0300

    feat: news list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+import { NewsService } from '../../services/news.service';
+import { INews } from '../../services/news.interface';
+
+const PAGE_SIZE = 3;
+
+interface PageItems {
+  page: number;
+  items: INews[];
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private _destroyed$ = new Subject<boolean>();
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<', '>'],
+    responsive: {
+      0: { items: 1 },
+    },
+    nav: true,
+  };
+
+  news: INews[] = [];
+
+  pageItems: PageItems[] = [];
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.newsService
+      .getNews()
+      .pipe(takeUntil(this._destroyed$))
+      .subscribe((news) => {
+        this.news = news;
+        const pageCount = Math.ceil(this.news.length / PAGE_SIZE);
+
+        for (let i = 0; i < pageCount; i++) {
+          this.pageItems.push({ page: i, items: this.getItemsByPage(i) });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next(true);
+    this._destroyed$.complete();
+  }
+
+  getItemsByPage(page: number) {
+    const start = page * PAGE_SIZE;
+
+    return this.news.slice(start, start + PAGE_SIZE);
+  }
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    CarouselModule,
+    HttpClientModule,
+    SharedModule,
+  ],
 })
 export class HomeModule {}
