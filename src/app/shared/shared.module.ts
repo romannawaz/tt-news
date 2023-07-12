e056ce1d41daaf9ddb637a0315d37964cd07913c
romannawaz <romannawaz01@gmail.com>
2023-07-12 18:14:59 +0300

    feat(news.componnet): added link to news page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -9,7 +10,7 @@ const Components = [NewsComponent];
 
 @NgModule({
   declarations: [...Components],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   exports: [...Components],
 })
 export class SharedModule {}
