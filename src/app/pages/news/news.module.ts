e47f4b8cb17252b282b1517fc5cd8a2ae3f4aa56
romannawaz <romannawaz01@gmail.com>
2023-07-12 18:26:46 +0300

    feat(news.page): added layout

--- src/app/pages/news/news.module.ts
+++ src/app/pages/news/news.module.ts
@@ -4,9 +4,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { NewsRoutingModule } from './news-routing.module';
 import { NewsComponent } from './news.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [NewsComponent],
-  imports: [CommonModule, NewsRoutingModule, FontAwesomeModule],
+  imports: [CommonModule, NewsRoutingModule, FontAwesomeModule, SharedModule],
 })
 export class NewsModule {}
